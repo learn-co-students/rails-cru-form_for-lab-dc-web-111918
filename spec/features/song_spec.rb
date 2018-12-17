bb78e7ea50546f04ed9601a62755766ec9669bad
github username <github email address>
2018-12-17 17:46:03 -0500

    Done.

--- spec/features/song_spec.rb
+++ spec/features/song_spec.rb
@@ -12,15 +12,16 @@ describe 'navigate' do
     expect(page).to have_css("h1", text: "My Song")
   end
 
-  it 'displays a link to the genre page' do 
+  it 'displays a link to the genre page' do
     visit song_path(@song.id)
+###failing this one
 
     expect(page).to have_link(@genre.name, href: genre_path(@genre))
   end
 
-  it 'displays a link to the artist page' do 
+  it 'displays a link to the artist page' do
     visit song_path(@song.id)
-
+###failing this one
     expect(page).to have_link(@artist.name, href: artist_path(@artist))
   end
 
@@ -75,5 +76,5 @@ describe 'index' do
     expect(page).to have_css("p", text: "My Song")
   end
 
-  
+
 end
